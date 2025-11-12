80e170917f3b9dfe4688c2aabdee7e1f227d9a05
Jero Balucas <jero.paolo@gmail.com>
2025-11-12 23:17:19 +0800

    Add news module

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
 
-const News = () => {
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const demoImage =
+  "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
+
+const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: newsCategory,
+    count: simplified ? 6 : 12,
+  });
+  const { data } = useGetCryptosQuery(100);
+
+  if (!cryptoNews?.data) return "Loading...";
+
   return (
-    <div>News</div>
-  )
-}
+    <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) => console.log(input, option)
+              // option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Option value="Cryptocurrency"></Option>
+            {data?.data?.coins.map((coin) => (
+              <Option value={coin.name}>{coin.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
+      {cryptoNews?.data.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.title}
+                </Title>
+                <img
+                  style={{ maxWidth: "100px", maxHeight: "100px" }}
+                  src={news?.img || demoImage}
+                  alt="news"
+                />
+              </div>
+              <p>
+                {news.desc > 100
+                  ? `${news.desc.substring(0, 100)}...`
+                  : news.desc}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar src={news?.image || demoImage} alt="" />
+                  <Text className="provider-name">
+                    {news.source}
+                  </Text>
+                </div>
+                {/* <Text>
+                  {moment(news.datePublished).startOf("ss").fromNow()}
+                </Text> */}
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
-export default News
+export default News;
